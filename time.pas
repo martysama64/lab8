f0736303e3071154a89a0e801375c8ca26aaadef
Мартирос Сагакянц <martisagak@gmail.com>
2023-09-30 10:48:35 +0300

    timer

---
+++ time.pas
@@ -0,0 +1,6 @@
+﻿begin
+  var A:=ReadInteger('Введите год:');
+  if A.Divs(4) and not(not(A.Divs(400)) and A.divs(100)) then
+    Print('Это високосный год!!!')
+  else  Print('Это не високосный год...');
+end.
